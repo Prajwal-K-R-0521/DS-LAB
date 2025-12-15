1a09d3416e5dd07bef064f68896cab120330f58b
Prajwal-K-R-0521 <prajwalkr.cs24@bmsce.ac.in>
2025-12-15 09:52:56 +0530

    Add files via upload

---
+++ QUEUE OPERATION/queueop.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int queue[MAX];
+int front = -1;
+int rear = -1;
+
+void insert() {
+    int value;
+
+    if (rear == MAX - 1) {
+        printf("Queue Overflow! Cannot insert element.\n");
+    } else {
+        if (front == -1)
+            front = 0;
+
+        printf("Enter value to insert: ");
+        scanf("%d", &value);
+
+        rear++;
+        queue[rear] = value;
+
+        printf("%d inserted into queue.\n", value);
+    }
+}
+
+void delete() {
+    if (front == -1 || front > rear) {
+        printf("Queue is empty! Cannot delete element.\n");
+    } else {
+        printf("%d deleted from queue.\n", queue[front]);
+        front++;
+
+        if (front > rear) {
+            front = -1;
+            rear = -1;
+        }
+    }
+}
+
+void display() {
+    int i;
+
+    if (front == -1) {
+        printf("Queue is empty.\n");
+    } else {
+        printf("Queue elements are:\n");
+        for (i = front; i <= rear; i++) {
+            printf("%d ", queue[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int choice;
+
+    do {
+        printf("\n--- Queue Menu ---\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                insert();
+                break;
+            case 2:
+                delete();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                printf("Exiting program.\n");
+                break;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
